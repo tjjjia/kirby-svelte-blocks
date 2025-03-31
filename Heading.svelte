5d1cb95f4c17c6d9add54e679d8f3c91c87c93fb
tjia.work <gitlab@tjia.work>
2025-03-31 14:05:20 +0200

    init repo

---
+++ Heading.svelte
@@ -0,0 +1,23 @@
+<script>
+	/* Heading
+	based on Kirby implementation
+	https://getkirby.com/docs/reference/panel/blocks/heading
+	
+	*/
+
+	export let block = {
+		content: {
+			level: "h2",
+			text: ""
+		},
+		id: "",
+		isHidden: true,
+		type: "heading"
+	};
+
+	const { level, text } = block.content;
+</script>
+
+{#if !block.isHidden}
+	<svelte:element this={level}>{text}</svelte:element>
+{/if}
